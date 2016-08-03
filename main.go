a344fb9e46e205e27617254b531040612d70889a
MatthewDooler <MatthewDooler@users.noreply.github.com>
2016-08-03 20:37:49 +0100

    config!

--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import (
     "log"
     "net/http"
+    "encoding/json"
+    "os"
 )
+
+type Configuration struct {
+    Port        string
+    BaseURI     string
+}
 
 func main() {
 
+    config := loadConfig()
     router := NewRouter()
 
-    log.Fatal(http.ListenAndServe(":8042", router)) // TODO: move port into config
+    log.Fatal(http.ListenAndServe(":"+config.Port, router))
+}
+
+func loadConfig() Configuration {
+    file, _ := os.Open("config.json")
+    decoder := json.NewDecoder(file)
+    configuration := Configuration{}
+    err := decoder.Decode(&configuration)
+    if err != nil {
+        log.Fatal("error loading config: ", err)
+    }
+    return configuration
 }
