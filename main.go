b80b110541b1358b4c9fe059dd750cf1535c8715
Matthew Dooler <dooler.matthew@gmail.com>
2016-08-12 18:45:40 +0100

    put db config in config file and added an overrides file

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 type Configuration struct {
     Port        string
     BaseURI     string
+    DBHost      string
+    DBBucket    string
+    DBPassword  string
 }
 
 var config Configuration
@@ -17,21 +20,25 @@ var dbContext DBContext
 
 func main() {
 
-    db, err := dbConnect()
+    config = loadConfig("config.json")
+    configOverrides := loadConfig("config-overrides.json")
+    config = applyConfigOverrides(config, configOverrides)
+
+    db, err := dbConnect(config.DBHost, config.DBBucket, config.DBPassword)
     dbContext = db
     if err != nil {
         log.Printf("error connecting to database: %s", err)
         return
     }
-    config = loadConfig()
+
     router := NewRouter()
 
     log.Printf("Starting server on 127.0.0.1:%s", config.Port)
     log.Fatal(http.ListenAndServe(":"+config.Port, router))
 }
 
-func loadConfig() Configuration {
-    file, _ := os.Open("config.json")
+func loadConfig(filename string) Configuration {
+    file, _ := os.Open(filename)
     decoder := json.NewDecoder(file)
     configuration := Configuration{}
     err := decoder.Decode(&configuration)
@@ -39,4 +46,14 @@ func loadConfig() Configuration {
         log.Fatal("error loading config: ", err)
     }
     return configuration
+}
+
+func applyConfigOverrides(config Configuration, configOverrides Configuration) Configuration {
+    // TODO: use reflection
+    if configOverrides.Port != "" { config.Port = configOverrides.Port }
+    if configOverrides.BaseURI != "" { config.BaseURI = configOverrides.BaseURI }
+    if configOverrides.DBHost != "" { config.DBHost = configOverrides.DBHost }
+    if configOverrides.DBBucket != "" { config.DBBucket = configOverrides.DBBucket }
+    if configOverrides.DBPassword != "" { config.DBPassword = configOverrides.DBPassword }
+    return config
 }
